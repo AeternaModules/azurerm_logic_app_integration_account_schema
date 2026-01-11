10e94d01b8d29697a9788c1000ec914bfbfc862b
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:42:56 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_integration_account_schemas" {
+  description = "All logic_app_integration_account_schema resources"
+  value       = azurerm_logic_app_integration_account_schema.logic_app_integration_account_schemas
+}
+
